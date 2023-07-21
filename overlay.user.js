69187a6dbc5f4d24a720e2794c36565c32b2a20f
Chris-GW <Chris-GW@users.noreply.github.com>
2023-07-21 15:11:04 +0200

    overlay.user.js aktualisiert nun alle 30s automatisch das neuste overlay.png (Kein manuelles F5 mehr notwendig). Wahrscheinlich ist aber der GitHub Cache von 5 min unsere Wartezeit. Dies können wir nur umgehen, wenn die overlay.png auf einen eigenen Server liegt.

--- overlay.user.js
+++ overlay.user.js
@@ -1,33 +1,32 @@
 // ==UserScript==
 // @name         Bonjwa + r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      7
+// @version      9
 // @description  try to take over the canvas! Combination of Bonjwa and r/placeDE template
 // @author       Chris-GW, nama17, Kloroller_DE, vertigo, Sockenschuh
 // @match        https://garlic-bread.reddit.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.bonjwa.de
 // @updateURL    https://github.com/rplacebonjwa/rplace/raw/main/overlay.user.js
 // @downloadURL  https://github.com/rplacebonjwa/rplace/raw/main/overlay.user.js
 // ==/UserScript==
-let url = "https://raw.githubusercontent.com/rplacebonjwa/rplace/main/overlay.png"
-let url_de = "https://place.army/overlay_target.png"
 
-if (window.top !== window.self) {
+addOverlayImage("https://raw.githubusercontent.com/rplacebonjwa/rplace/main/overlay.png")
+addOverlayImage("https://place.army/overlay_target.png")
+
+function addOverlayImage(url) {
+  var overlayImage = null;
+  if (window.top !== window.self) {
     window.addEventListener('load', () => {
-        console.log("Bonjwa + r/placeDE Template loaded", document.getElementsByTagName("garlic-bread-embed"));
-        document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
-            (function () {
-                const i = document.createElement("img");
-                i.src = url;
-                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
-                return i;
-            })());
-        document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
-            (function () {
-                const i = document.createElement("img");
-                i.src = url_de;
-                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
-                return i;
-            })());
+      const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
+      overlayImage = document.createElement("img");
+      overlayImage.src = url + "?" + Date.now()
+      overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;pointerEvents: 'none';`;
+      canvasContainer.appendChild(overlayImage);
     }, false);
+  }
+
+  setInterval(function () {
+    overlayImage.src = url + "?" + Date.now()
+  }, 30000);
 }
+
