f6a16e7e573eb789ce30beb79307d1373618be61
Chris-GW <Chris-GW@users.noreply.github.com>
2023-07-21 23:20:32 +0200

    troll Rick Astley im Overlay

--- overlay.user.js
+++ overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bonjwa + r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      11
+// @version      12
 // @description  try to take over the canvas! Combination of Bonjwa and r/placeDE template
 // @author       Chris-GW, nama17, Kloroller_DE, vertigo, Sockenschuh
 // @match        https://garlic-bread.reddit.com/*
