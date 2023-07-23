81a9be983a6f2eeed16a41ef6c374272c4d945ed
Chrimi8 <Chrimi8@users.noreply.github.com>
2023-07-23 22:54:54 +0200

    overlay.user.js Version hochzaehlen

--- overlay.user.js
+++ overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bonjwa + r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      16
+// @version      17
 // @description  try to take over the canvas! Combination of Bonjwa and r/placeDE template
 // @author       Chris-GW, nama17, Kloroller_DE, vertigo, Sockenschuh, Chrimi8
 // @match        https://garlic-bread.reddit.com/*
