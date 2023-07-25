693b8b5cfe94afcd3188716da403e355f5ceff25
Deimos <deimos.game@live.com>
2023-07-25 21:17:05 +0200

    new button to show output image

--- overlay.user.js
+++ overlay.user.js
@@ -22,10 +22,15 @@ let overlayUrls = [
   "https://raw.githubusercontent.com/rplacebonjwa/rplace/main/overlay.png",
 ];
 
+let outputImageUrls = [
+  // Gronkh output
+  "https://raw.githubusercontent.com/FeLuckLP/rplace/main/output.png",
+  // eigenes output
+  "https://raw.githubusercontent.com/rplacebonjwa/rplace/main/output.png",
+];
+
 let overlayContext = null;
 let overlayImage = null;
-
-const mainContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector(".layout");
 
 function exportScreenshot() {
   const canvas = mainContainer.querySelector("garlic-bread-canvas").shadowRoot.querySelector("canvas");
@@ -54,6 +59,7 @@ function readStorage(name) {
 }
 
 let hideOverlay = readStorage("hideOverlay") === "true" ? true : false;
+let hideOutputImage = readStorage("hideOutputImage") === "true" ? true : false;
 
 function addOverlay() {
   if (window.top !== window.self) {
@@ -92,29 +98,38 @@ function repaintOverlays() {
   overlayUrls.forEach((url) => {
     let img = new Image();
     img.onload = function () {
+      overlayContext.imageSmoothingEnabled = false;
       overlayContext.drawImage(img, 0, 0);
     };
     img.src = url + "?" + Date.now();
   });
+
+  if (!hideOutputImage) {
+    outputImageUrls.forEach((url) => {
+      let img = new Image();
+      img.onload = function () {
+        overlayContext.imageSmoothingEnabled = false;
+        overlayContext.drawImage(img, 0, 0, canvasWidth * 3, canvasHeight * 3);
+      };
+      img.src = url + "?" + Date.now();
+    });
+  }
 }
 
 function updateHideButtonIcon(hide) {
   let divElement = document.getElementById("hideIcon");
 
-  if (!hide) {
-    divElement.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-          <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-          <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
-        </svg>`;
-  } else {
-    divElement.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-        <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
-        <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
-        <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
-      </svg>`;
-  }
+  if (!hide)
+    divElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path d="M12 15a3 3 0 100-6 3 3 0 000 6z" /><path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" /></svg>`;
+  else
+    divElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" /><path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" /><path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" /></svg>`;
+}
+
+function updateOutputImageButtonIcon(hide) {
+  let icon = document.getElementById("imageIcon");
+
+  if (hide) icon.style.color = "#a3a3a3";
+  else icon.style.color = "#22c55e";
 }
 
 function createModal(text, showUpdateButton = false) {
@@ -251,11 +266,22 @@ function createCompleteButton(pos) {
   return button;
 }
 
+function restartInterval() {
+  clearInterval(interval);
+  repaintOverlays();
+  interval = setInterval(repaintOverlays, 90 * 1000);
+}
+
+const mainContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector(".layout");
+addOverlay();
+let interval = setInterval(repaintOverlays, 90 * 1000);
+
 if (window.top !== window.self) {
   const container = document.querySelector("garlic-bread-embed");
+  let buttonCounter = 1;
 
   // create button to show / hide overlay
-  let showHideButton = createCompleteButton(1);
+  let showHideButton = createCompleteButton(buttonCounter++);
   showHideButton.onclick = function () {
     hideOverlay = !hideOverlay;
     updateHideButtonIcon(hideOverlay);
@@ -266,16 +292,28 @@ if (window.top !== window.self) {
   container.appendChild(showHideButton);
   updateHideButtonIcon(hideOverlay);
 
+  // create button to show the output image
+  let outputImageButton = createCompleteButton(buttonCounter++);
+  outputImageButton.onclick = function () {
+    hideOutputImage = !hideOutputImage;
+    updateOutputImageButtonIcon(hideOutputImage);
+    saveStorage("hideOutputImage", hideOutputImage);
+    restartInterval();
+  };
+  outputImageButton.firstChild.firstChild.innerHTML = `<svg id="imageIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" /></svg>`;
+  container.appendChild(outputImageButton);
+  updateOutputImageButtonIcon(hideOutputImage);
+
   // create button to download a screenshot
-  let screenshotButton = createCompleteButton(2);
+  let screenshotButton = createCompleteButton(buttonCounter++);
   screenshotButton.onclick = function () {
     exportScreenshot();
   };
-  screenshotButton.firstChild.firstChild.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2 3)"><path d="M20 16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h3l2-3h6l2 3h3a2 2 0 0 1 2 2v11z"/><circle cx="10" cy="10" r="4"/></g></svg> `;
+  screenshotButton.firstChild.firstChild.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2 3)"><path d="M20 16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h3l2-3h6l2 3h3a2 2 0 0 1 2 2v11z"/><circle cx="10" cy="10" r="4"/></g></svg>`;
   container.appendChild(screenshotButton);
 
   // create button to check if new update available
-  let updateButton = createCompleteButton(3);
+  let updateButton = createCompleteButton(buttonCounter++);
   updateButton.id = "hideButton";
   updateButton.onclick = function () {
     checkForUpdates(true);
@@ -283,12 +321,3 @@ if (window.top !== window.self) {
   updateButton.firstChild.firstChild.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"> <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clipRule="evenodd" style="stroke-width: 3px;" /></svg>`;
   container.appendChild(updateButton);
 }
-
-function restartInterval() {
-  clearInterval(interval);
-  repaintOverlays();
-  interval = setInterval(repaintOverlays, 90 * 1000);
-}
-
-addOverlay();
-let interval = setInterval(repaintOverlays, 90 * 1000);
